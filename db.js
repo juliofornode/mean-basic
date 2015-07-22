2c167ab12af0a7d4ee14a3c7fb81a1bde9bba5b8
Julio <juliofornode@gmail.com>
2015-07-22 13:19:52 -0700

    changed db connect to mongolab

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/dbone', function() {
+mongoose.connect('mongodb://ds047478.mongolab.com:47478/heroku_9bfj9t84', function() {
 	console.log('mongodb connected')
 })
 
 module.exports = mongoose
-
