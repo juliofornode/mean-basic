369de5cf0e055693a473a2888f1c7fc298f473eb
Julio <juliofornode@gmail.com>
2015-07-22 13:33:07 -0700

    changed db connect to mongolab 3

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-var url = process.env.mongodb://heroku_9bfj9t84:8r7jh6i81t7am1subec7cbhm5t@ds047478.mongolab.com:47478/heroku_9bfj9t84 || 'mongodb://localhost/dbone'
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost/dbone'
 
 mongoose.connect(url)
 
