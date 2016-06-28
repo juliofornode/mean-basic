96d51e9c22d3a14f71b32eadde8cc854ac3b4a17
Julio <juliofornode@gmail.com>
2016-06-28 12:17:53 -0700

    updated mongoose version

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 var url = 'mongodb://bob:test@ds023684.mlab.com:23684/tweety'
 //var url = 'mongodb://localhost/dbone'
 
-mongoose.connect('mongodb://bob:test@ds023684.mlab.com:23684/tweety', function(err) {
+mongoose.connect(url, function(err) {
     if(err) {
         console.log(err);
     } else {
