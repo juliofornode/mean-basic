8e19467b96f35bb7b6dd19e099c80e3794aa7beb
Julio <juliofornode@gmail.com>
2016-06-28 12:04:04 -0700

    updated mongoose

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
 var mongoose = require('mongoose')
-var url = 'mongodb://julio:test@ds023684.mlab.com:23684/tweety';
+var url = 'mongodb://bob:test@ds023684.mlab.com:23684/tweety'
 //var url = 'mongodb://localhost/dbone'
 
-mongoose.connect(url)
+mongoose.connect('mongodb://bob:test@ds023684.mlab.com:23684/tweety', function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log('db connected');
+    }
+});
 
 module.exports = mongoose
 
